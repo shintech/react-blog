9db7fc76106b287dbb721c2bee6dd3573530a479
Michael Prather <mprather1@gmail.com>
2017-10-17 16:37:08 +0000

    add-some-fake-services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import faker from 'faker'
 
 export default class Services extends React.Component {
   render () {
     return (
       <div>
         <ul>
-          <li>Service 1</li>
-          <li>Service 2</li>
-          <li>Service 3</li>
+          <li>
+            <h3>{faker.fake('{{hacker.ingverb}}')}</h3>
+            <p>{faker.fake('{{hacker.phrase}}')}</p>
+          </li>
+          <li>
+            <h3>{faker.fake('{{hacker.ingverb}}')}</h3>
+            <p>{faker.fake('{{hacker.phrase}}')}</p>
+          </li>
+          <li>
+            <h3>{faker.fake('{{hacker.ingverb}}')}</h3>
+            <p>{faker.fake('{{hacker.phrase}}')}</p>
+          </li>
+          <li>
+            <h3>{faker.fake('{{hacker.ingverb}}')}</h3>
+            <p>{faker.fake('{{hacker.phrase}}')}</p>
+          </li>
+          <li>
+            <h3>{faker.fake('{{hacker.ingverb}}')}</h3>
+            <p>{faker.fake('{{hacker.phrase}}')}</p>
+          </li>
         </ul>
       </div>
     )
